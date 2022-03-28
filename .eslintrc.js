908ff856ed986d5b4f8339d42ad7337933c4f4fa
OlegKhiretdinov <oleg@oleg.win>
2022-03-28 19:48:10 +0300

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks"],
+  rules: {
+    "react/prop-types": [0],
+    "react/self-closing-comp": "error",
+    "no-console": "error",
+    "react-hooks/rules-of-hooks": "error",
+  },
+};
