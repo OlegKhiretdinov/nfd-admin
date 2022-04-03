e3c28de680230054996de392b41cc0d3b01846e3
OlegKhiretdinov <oleg@oleg.win>
2022-04-03 18:17:32 +0300

    add HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom"
 import AdminPage from "./components/AdminPage/AdminPage"
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/admin" element={<AdminPage />} />
         <Route exact path="/" element={<Navigate to="/admin" />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
